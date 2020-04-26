29126a991169f6c666dd8bc514f19c688144e82b
rohitpjpti18 <rohitpjpti18@gmail.com>
2020-04-26 17:16:06 +0530

    modified BoxDemo

--- basics/classes-and-objects/BoxDemo.java
+++ basics/classes-and-objects/BoxDemo.java
@@ -4,20 +4,40 @@ class Box {
     double width;
     double height;
     double depth;
+
+
+    Box() {
+        System.out.println("Constructing Box");
+        this.width = 10;
+        this.height = 10;
+        this.depth = 10;
+    }
+
+    Box(double s) {
+        this.width = s;
+        this.height = s;
+        this.depth = s;
+    }
+
+    Box(double w, double h, double d){
+        this.width = w;
+        this.height = h;
+        this.depth = d;
+    }
+
+
+    double volume() {
+        return width * height * depth;
+    }
 }
 
 
 class BoxDemo {
     public static void main(String args[]) {
         Box mybox = new Box();
-        double vol;
-        
-        mybox.width = 10;
-        mybox.height = 20;
-        mybox.depth = 15;
 
-        vol = mybox.width * mybox.height * mybox.depth;
 
-        System.out.println("volume is " + vol);
+
+        System.out.println(mybox.volume());
     }
 }

865bbcc05ba2a5daf10a0afa4fd75edbe933ea91
rohitpjpti18 <rohitpjpti18@gmail.com>
2020-04-26 10:14:00 +0530

    added LicenseServiceController:getLicenses

--- spring-boot/spmia-chapter2/licensing-service/src/main/java/com/thoughtmechanix/licenses/controllers/LicenseServiceController.java
+++ spring-boot/spmia-chapter2/licensing-service/src/main/java/com/thoughtmechanix/licenses/controllers/LicenseServiceController.java
@@ -13,34 +13,16 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 @RestController
 @RequestMapping(value="v1/organizations/{organizationId}/licenses")
 public class LicenseServiceController {
-    @Autowired
-    private LicenseService licenseService;
 
-    @RequestMapping(value="/{licenseId}",method = RequestMethod.GET)
-    public License getLicenses( @PathVariable("organizationId") String organizationId,
-                                @PathVariable("licenseId") String licenseId) {
-
-        //return licenseService.getLicense(licenseId);
+    @RequestMapping(value="/{licenseId}", method=RequestMethod.GET)
+    public License getLicenses(
+        @PathVariable("organizationId") String organizationId, 
+        @PathVariable("licenseId") String licenseId){
+        
         return new License()
             .withId(licenseId)
-            .withOrganizationId(organizationId)
             .withProductName("Teleco")
-            .withLicenseType("Seat");
-    }
-
-    @RequestMapping(value="{licenseId}",method = RequestMethod.PUT)
-    public String updateLicenses( @PathVariable("licenseId") String licenseId) {
-        return String.format("This is the put");
-    }
-
-    @RequestMapping(value="{licenseId}",method = RequestMethod.POST)
-    public String saveLicenses( @PathVariable("licenseId") String licenseId) {
-        return String.format("This is the post");
-    }
-
-    @RequestMapping(value="{licenseId}",method = RequestMethod.DELETE)
-    @ResponseStatus(HttpStatus.NO_CONTENT)
-    public String deleteLicenses( @PathVariable("licenseId") String licenseId) {
-        return String.format("This is the Delete");
+            .withLicenseType("Seat")
+            .withOrganizationId("TestOrg");
     }
 }

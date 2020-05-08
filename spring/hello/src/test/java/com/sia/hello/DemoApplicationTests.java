4cc3ef8f78effbc1c2b145a28a575048d367bfca
rohitpjpti18 <rohitpjpti18@gmail.com>
2020-05-08 09:48:49 +0530

    initiated new project hello

---
+++ spring/hello/src/test/java/com/sia/hello/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sia.hello;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

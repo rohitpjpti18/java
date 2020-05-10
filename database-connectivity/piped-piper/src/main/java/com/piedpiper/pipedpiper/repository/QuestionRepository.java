5f49bb0f6950037615cb33bb7bb8ad099e7c0711
rohitpjpti18 <rohitpjpti18@gmail.com>
2020-05-10 08:06:06 +0530

    deleted database-connectivity

--- database-connectivity/piped-piper/src/main/java/com/piedpiper/pipedpiper/repository/QuestionRepository.java
+++
@@ -1,9 +0,0 @@
-package com.piedpiper.pipedpiper.repository;
-
-import com.piedpiper.pipedpiper.model.Question;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface QuestionRepository extends JpaRepository<Question, Long> {
-}

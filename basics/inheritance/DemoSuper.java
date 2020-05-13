7261e9f14d5f65c8a7536e1733e73a55936e721d
rohitpjpti18 <rohitpjpti18@gmail.com>
2020-05-13 10:01:15 +0530

    added DemoSuper

---
+++ basics/inheritance/DemoSuper.java
@@ -0,0 +1,96 @@
+class Box {
+    private double width;
+    private double height;
+    private double depth;
+
+    // construct clone of an object
+    Box(Box ob){
+        width = ob.width;
+        height = ob.height;
+        depth = ob.depth;
+    }
+
+    Box(double w, double h, double d) {
+        width = w;
+        height = h;
+        depth = d;
+    }
+
+    Box() {
+        width = -1;
+        height = -1;
+        depth = -1;
+    }
+
+    Box(double len) {
+        width = height = depth = len;
+    }
+
+    double volume() {
+        return width * height * depth;
+    }
+}
+
+
+class BoxWeight extends Box { //
+    double weight; 
+    
+    // construct clone of an object
+    BoxWeight(BoxWeight ob){
+        super(ob);
+        weight = ob.weight;
+    }
+
+    // constructor when all parameters are specified
+    BoxWeight(double w, double h, double d, double m) {
+        super(w, h, d);
+        weight = m;
+    }
+
+    BoxWeight() {
+        super();
+        weight = -1;
+    }
+
+    BoxWeight(double len, double m) {
+        super(len);
+        weight = m;
+    }    
+}
+
+
+class DemoSuper {
+    public static void main(String[] args) {
+        BoxWeight mybox1 = new BoxWeight(10, 20, 15, 34.3);
+        BoxWeight mybox2 = new BoxWeight(2, 3, 4, 0.076);
+        BoxWeight mybox3 = new BoxWeight();
+        BoxWeight mycube = new BoxWeight(3, 2);
+        BoxWeight myclone = new BoxWeight(mybox1);
+        double vol;
+
+        vol = mybox1.volume();
+        System.out.println("volume of mybox1 is " + vol);
+        System.out.println("weight of mybox1 is " + mybox1.weight);
+        System.out.println();
+
+        vol = mybox2.volume();
+        System.out.println("volume of mybox2 is " + vol);
+        System.out.println("weight of mybox2 is " + mybox2.weight);
+        System.out.println();
+
+        vol = mybox3.volume();
+        System.out.println("volume of mybox3 is " + vol);
+        System.out.println("weight of mybox3 is " + mybox3.weight);
+        System.out.println();
+
+        vol = myclone.volume();
+        System.out.println("volume of myclone is " + vol);
+        System.out.println("weight of myclone is " + myclone.weight);
+        System.out.println();
+
+        vol = mycube.volume();
+        System.out.println("volume of mycube is " + vol);
+        System.out.println("weight of mycube is " + mycube.weight);
+        System.out.println();
+    }
+}
